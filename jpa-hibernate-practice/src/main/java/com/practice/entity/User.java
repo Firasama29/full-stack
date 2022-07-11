3c1d953464962f055cf8535e27aff7b26884495f
Firasama29 <ferasama@gmail.com>
2022-07-11 11:26:56 +0800

    jpa-hibernate practice project

---
+++ jpa-hibernate-practice/src/main/java/com/practice/entity/User.java
@@ -0,0 +1,38 @@
+package com.practice.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    private int userId;
+    @Column(name = "user_name")
+    private String userName;
+    public User(){}
+
+    public User(int userId, String userName) {
+        this.userId = userId;
+        this.userName = userName;
+    }
+
+    public int getUserId() {
+        return this.userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return this.userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+}
---
+++ jpa-hibernate-practice/src/main/java/com/practice/service/EntityManagerService.java
@@ -0,0 +1,13 @@
+package com.practice.service;
+
+import com.practice.entity.User;
+
+import javax.persistence.EntityManager;
+
+public class EntityManagerService {
+
+    User user = new User();
+
+    EntityManager em;
+
+}
