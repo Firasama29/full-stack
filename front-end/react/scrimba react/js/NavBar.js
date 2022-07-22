42e3866301045d5994a841d64ed8eccc170be33e
Firasama29 <ferasama@gmail.com>
2022-07-22 19:09:07 +0800

    update front-end folder

---
+++ front-end/react/scrimba react/js/NavBar.js
@@ -0,0 +1,12 @@
+export default function NavBar() {
+    return (
+        <nav className="nav">
+            <img src="logo-og.png" className="nav-logo"/>
+            <ul className="nav-style">
+                <li>Pricing</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+}
